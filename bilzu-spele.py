54a80a8430be2018d65fd10861b6ec18671540e2
annapanjuti <anna.panjuti15@gmail.com>
2023-12-13 19:19:22 +0200

    Update bilzu-spele.py
    13.12.2023. mājās

--- bilzu-spele.py
+++ bilzu-spele.py
@@ -3,37 +3,35 @@ from PIL import ImageTk,Image
 import random
 from tkinter import messagebox
 
+
 gameWindow=Tk()
 gameWindow.title("Vienādie attēli")
+gameWindow.resizable(width=False, height=False)
 #count=0
 #Answcount=0
 
 #izveido pogas - jābūt pāra skaitlim, piemēram, 10
-btn0=Button(width=4,height=3) #sāk no 0, jo sasaistās ar saraksta elementu indeksāciju
-btn1=Button(width=4,height=3)
-btn2=Button(width=4,height=3)
-btn3=Button(width=4,height=3)
-btn4=Button(width=4,height=3)
-btn5=Button(width=4,height=3)
-btn6=Button(width=4,height=3)
-btn7=Button(width=4,height=3)
-btn8=Button(width=4,height=3)
-btn9=Button(width=4,height=3)
-btn10=Button(width=4,height=3)
-btn11=Button(width=4,height=3)
+btn0=Button(width=15,height=5) #sāk no 0, jo sasaistās ar saraksta elementu indeksāciju
+btn1=Button(width=15,height=5)
+btn2=Button(width=15,height=5)
+btn3=Button(width=15,height=5)
+btn4=Button(width=15,height=5)
+btn5=Button(width=15,height=5)
+btn6=Button(width=15,height=5)
+btn7=Button(width=15,height=5)
+btn8=Button(width=15,height=5)
+btn9=Button(width=15,height=5)
 
 btn0.grid(row=0,column=0) #sāk no 0, jo sasaistās ar saraksta elementu indeksāciju
 btn1.grid(row=0,column=1)
 btn2.grid(row=0,column=2)
-btn3.grid(row=1,column=0)
-btn4.grid(row=1,column=1)
-btn5.grid(row=1,column=2)
-btn6.grid(row=2,column=0)
-btn7.grid(row=2,column=1)
-btn8.grid(row=2,column=2)
-btn9.grid(row=3,column=0)
-btn10.grid(row=3,column=1)
-btn11.grid(row=3,column=2)
+btn3.grid(row=0,column=3)
+btn4.grid(row=0,column=4)
+btn5.grid(row=1,column=0)
+btn6.grid(row=1,column=1)
+btn7.grid(row=1,column=2)
+btn8.grid(row=1,column=3)
+btn9.grid(row=1,column=4)
 #saliek attēlus saraksta, katrs attēls 2 reizes
 
 bulterjers=ImageTk.PhotoImage(Image.open("bulterjers.png"))
@@ -43,7 +41,7 @@ keribluterjers=ImageTk.PhotoImage(Image.open("keriblu-terjers.jpg"))
 kviesuterjers=ImageTk.PhotoImage(Image.open("kviesu-terjers.jpg"))
 
 #FONA ATTĒLS
-bgImg=Image.Tk.PhotoImage(Image.open("kauls.jpg").resize((200,200), Image.ANTIALIAS)) #attēlu saspiešanas metode
+#bgImg=Image.Tk.PhotoImage(Image.open("kauls.jpg").resize((200,200), Image.ANTIALIAS)) #attēlu saspiešanas metode
 
 #resize((x,y))
 gameWindow.mainloop()
